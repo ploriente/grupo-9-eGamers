881a3e8b07539a41117c7a00308439376beded95
ploriente <ploriente@yahoo.com>
2023-01-17 01:24:36 -0300

    proyecto actualizado hasta sprint 4

---
+++ src/controllers/mainController.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+/* En la constante "products" ya tienen los productos que están 
+guardados en la carpeta Data como Json (un array de objetos literales) */
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const controller = {
+	index: (req, res) => {
+		// Do the magic
+		const productosLeidos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+		let productosNuevosFiltrados = productosLeidos.filter( producto => {
+			return producto.newGame == true
+		})
+
+		let productosEnOfertaFiltrados = productosLeidos.filter( producto => {
+			return producto.inOffer == true
+		})
+
+		res.render("index", {
+			productosNuevos: productosNuevosFiltrados, 
+			productosEnOferta: productosEnOfertaFiltrados
+			}
+		);
+	},
+	search: (req, res) => {
+		// Do the magic
+	},
+};
+
+module.exports = controller;
