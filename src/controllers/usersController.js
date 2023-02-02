bc0b47ead83ddc685cb79d21bd3bd8db8fb45bb1
juanjocaba7 <juanjocabanellas@gmail.com>
2023-02-01 21:00:42 -0300

    Register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,14 +2,29 @@ const fs = require('fs')
 const express = require("express");
 const path = require("path")
 
+const { validationResult} = require("express-validator"); //express validator se requiere tantos en rutas y controlador
+
 
 const usersController = {
-    login: function(req, res){ 
-        res.render("./users/login")
-    },
     register: function(req, res){
         res.render("./users/register")
     },
-    
+    processRegister:(req,res) =>{//capturar las validaciones de rutas
+        const resultValidation = validationResult(req);
+//REsult validation no es un array, es un objeto literal que tiene la propiedad errors
+        if(resultValidation.errors.length > 0 ) {//quiere decir si hubiese errores
+            return res.render("./users/register", {//si hay errores se vuelve a mandar el formulario
+                errors: resultValidation.mapped(),//convertimos el array en un objeto literal
+                oldData: req.body
+            });
+        }//una vez registrado sin errores
+        return res.send("Ok, las validaciones se pasaron y no tienen errores")
+    },
+    login: function(req, res){ 
+        res.render("./users/login")
+    },
+    profile: (req, res) =>{
+        return res.render("./users/profile")
+    }
 }
 module.exports = usersController;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,66 @@
 
 //requires
 const express = require("express");
-const path = require("path")
-
 const router =express.Router()
 
-const usersController= require("../controllers/usersController")
+const path = require("path")//Este metododo contiene extname
+const multer = require("multer")// metodo para generar middlewares
 
-router.get("/login", usersController.login)
+const {body} =require("express-validator")// body es una función dentro del paquete express-validator
+
+const storage = multer.diskStorage({//disco donde se almacenara la informacion consta de estas dos propiedades con parametros
+    destination: (req, file, cb) =>{
+        cb(null, "./public/images/avatars")//guardamos img usuarios aca
+    },
+    filename: (req,file, cb)=>{
+        let fileName = `${Date.now()}_img${path.extname(file.originalname)}`
+        cb(null,fileName);
+    }
+})
+
+const uploadFile = multer({storage});//constante para generar el método a subir
+//storage necesita multer para subir el archivo
+
+const usersController= require("../controllers/usersController");
+
+//En validation creo todas las validaciones y la paso a las rutas de abajo para poder procesar los archivos;
+const validations = [
+    body("fullName").notEmpty().withMessage("Tienes que escribir un nombre"),
+    body("usuario").notEmpty().withMessage("Tienes que escribir un usuario"),
+    body("email")//bail corta las validaciones si no cumple con la primera
+        .notEmpty().withMessage("Tienes que escribir un correo electrónico").bail()
+        .isEmail().withMessage("Debes escribir un formato de correo válido"),
+    body("password").notEmpty().withMessage("Tienes que escribir una constraseña"),
+    body("country").notEmpty().withMessage("Tienes que elegir un país"),
+    body("avatar").custom((value, {req}) =>{//suncion que anailiza el tipo de imagen
+        let file = req.file;
+        let acceptedExtensions = [ ".jpg", ".png", ".git"];
+
+        if(!file){
+            throw new Error("Tienes que subir una imagen");
+        } else {
+            let fileExtension = path.extname(file.originalname); //cuando obtengo un archivo
+            if(!acceptedExtensions.includes(fileExtension)) {//valido el tipo de ese archivo
+                throw new Error(`Las extenciones de archivo permitidas son  ${acceptedExtensions.join(', ')}`);
+            }
+        }
+        return true;
+
+    })
+]
+
+//Formulario de registro
 router.get("/register", usersController.register)
+
+//Procesar el registro ///Agregando validations indicamos las condiciones de arriba
+router.post("/register", uploadFile.single("avatar"), validations, usersController.processRegister)
+//todo esto para procesar enctype multiform part data
+
+//Formulario de login
+router.get("/login", usersController.login);
+
+//Perfil de Usuario
+router.get("/profile/:userId", usersController.profile);
+
 
 module.exports = router;
